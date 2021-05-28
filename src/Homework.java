12a1cfae4f6e2086dccc1db9fc7b62f561495cc0
Andrey <andreybat2@gmail.com>
2021-05-28 12:47:11 +0300

    Homework 2

--- src/Homework.java
+++ src/Homework.java
@@ -1,5 +1,18 @@
 public class Homework {
-    public static void main(String[] arg){
-
+    public static void main(String[] args) {
+        try {
+            calculateSquare(1,5);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    public static int calculateSquare(int a, int b) throws Exception {
+        if (a <= 0 || b <= 0) {
+            throw new Exception("Переданные стороны некорректны!");
+        } else {
+            return a * b; }}
+
+
+
 }
